916812dbe824e90b26224f2f666d27289a5f6d0f
Kamil <ksikora312@gmail.com>
2021-06-12 22:28:44 +0200

    init commit

---
+++ src/main/java/eu/kamilsikora/financial/api/ManagementServiceApi.java
@@ -0,0 +1,13 @@
+package eu.kamilsikora.financial.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementServiceApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementServiceApi.class, args);
+    }
+
+}
