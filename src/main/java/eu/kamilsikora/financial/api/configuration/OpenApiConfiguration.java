a5244b514435ec895f2a7fc4c1d9d6dcfd34ac6b
Kamil <ksikora312@gmail.com>
2021-09-25 22:01:19 +0200

    Added authorization in swagger + some docs

--- src/main/java/eu/kamilsikora/financial/api/configuration/OpenApiConfiguration.java
+++ src/main/java/eu/kamilsikora/financial/api/configuration/OpenApiConfiguration.java
@@ -2,12 +2,23 @@ package eu.kamilsikora.financial.api.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -16,10 +27,39 @@ public class OpenApiConfiguration {
     @Bean
     public Docket swaggerApi() {
         return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
 
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Financial management service",
+                "Service provides API for managmenet of private finanses",
+                "1.0",
+                "Terms of service",
+                new Contact("Kamil Sikora", "kamilsikora.eu", "ksikora312@gmail.com"),
+                "GNU GPL",
+                "https://www.gnu.org",
+                Collections.emptyList());
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", HttpHeaders.AUTHORIZATION.toString(), "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
 }
