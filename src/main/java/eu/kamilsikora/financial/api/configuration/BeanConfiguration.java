a73ff348057504367cf18f2553bd1c50b6731d8e
Kamil <ksikora312@gmail.com>
2021-09-28 21:30:55 +0200

    Changed mail configuration to obtain username and password from environment variables

---
+++ src/main/java/eu/kamilsikora/financial/api/configuration/BeanConfiguration.java
@@ -0,0 +1,29 @@
+package eu.kamilsikora.financial.api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(System.getenv("mail"));
+        mailSender.setPassword(System.getenv("password"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+
+}
